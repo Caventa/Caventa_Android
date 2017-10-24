6e03d3d79efe6066485e5e4e4aae46a4b822a0f2
baneeishaque <baneeishaque@gmail.com>
2017-10-24 16:03:35 +0530

    RecyclerView

--- app/src/main/java/caventa/ansheer/ndk/caventa/Home.java
+++ app/src/main/java/caventa/ansheer/ndk/caventa/Home.java
@@ -4,6 +4,9 @@ import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -12,12 +15,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
 
@@ -88,38 +91,246 @@ public class Home extends AppCompatActivity {
 //        return super.onOptionsItemSelected(item);
 //    }
 
+    static int fin_data_flag = 0, pen_data_flag = 0, up_data_flag = 0;
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    public static class Pending_Works_Fragment extends Fragment {
 
-        public PlaceholderFragment() {
+
+        public Pending_Works_Fragment() {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
+        public static Pending_Works_Fragment newInstance() {
+            Pending_Works_Fragment fragment = new Pending_Works_Fragment();
             return fragment;
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            View rootView = inflater.inflate(R.layout.fragment_pending_works, container, false);
+
+            recyclerView = rootView.findViewById(R.id.recycler_view);
+
+            mAdapter = new MoviesAdapter(movieList);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+            recyclerView.setAdapter(mAdapter);
+
+            recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+                @Override
+                public void onClick(View view, int position) {
+                    Movie movie = movieList.get(position);
+                    Toast.makeText(getContext(), movie.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onLongClick(View view, int position) {
+
+                }
+            }));
+
+            if (pen_data_flag == 0) {
+                prepareMovieData();
+            }
+
             return rootView;
+        }
+
+        private List<Movie> movieList = new ArrayList<>();
+        private RecyclerView recyclerView;
+        private MoviesAdapter mAdapter;
+
+        private void prepareMovieData() {
+            Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
+            movieList.add(movie);
+
+            movie = new Movie("Inside Out", "Animation, Kids & Family", "2015");
+            movieList.add(movie);
+
+            movie = new Movie("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+            movieList.add(movie);
+
+            movie = new Movie("Shaun the Sheep", "Animation", "2015");
+            movieList.add(movie);
+
+            movie = new Movie("The Martian", "Science Fiction & Fantasy", "2015");
+            movieList.add(movie);
+
+            movie = new Movie("Mission: Impossible Rogue Nation", "Action", "2015");
+            movieList.add(movie);
+
+            mAdapter.notifyDataSetChanged();
+
+            pen_data_flag=1;
+        }
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class Finished_Works_Fragment extends Fragment {
+
+        public Finished_Works_Fragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Finished_Works_Fragment newInstance() {
+            Finished_Works_Fragment fragment = new Finished_Works_Fragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_finished_works, container, false);
+
+            recyclerView = rootView.findViewById(R.id.recycler_view);
+
+            mAdapter = new MoviesAdapter(movieList);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+            recyclerView.setAdapter(mAdapter);
+
+            recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+                @Override
+                public void onClick(View view, int position) {
+                    Movie movie = movieList.get(position);
+                    Toast.makeText(getContext(), movie.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onLongClick(View view, int position) {
+
+                }
+            }));
+
+            if (fin_data_flag == 0) {
+                prepareMovieData();
+            }
+
+            return rootView;
+        }
+
+        private List<Movie> movieList = new ArrayList<>();
+        private RecyclerView recyclerView;
+        private MoviesAdapter mAdapter;
+
+        private void prepareMovieData() {
+
+
+            Movie movie = new Movie("Up", "Animation", "2009");
+            movieList.add(movie);
+
+            movie = new Movie("Star Trek", "Science Fiction", "2009");
+            movieList.add(movie);
+
+            movie = new Movie("The LEGO Movie", "Animation", "2014");
+            movieList.add(movie);
+
+            movie = new Movie("Iron Man", "Action & Adventure", "2008");
+            movieList.add(movie);
+
+            movie = new Movie("Aliens", "Science Fiction", "1986");
+            movieList.add(movie);
+
+
+            mAdapter.notifyDataSetChanged();
+
+            fin_data_flag=1;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class Upcoming_Works_Fragment extends Fragment {
+
+        public Upcoming_Works_Fragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Upcoming_Works_Fragment newInstance() {
+            Upcoming_Works_Fragment fragment = new Upcoming_Works_Fragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_upcoming_works, container, false);
+
+            recyclerView = rootView.findViewById(R.id.recycler_view);
+
+            mAdapter = new MoviesAdapter(movieList);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+            recyclerView.setAdapter(mAdapter);
+
+            recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+                @Override
+                public void onClick(View view, int position) {
+                    Movie movie = movieList.get(position);
+                    Toast.makeText(getContext(), movie.getTitle() + " is selected!", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onLongClick(View view, int position) {
+
+                }
+            }));
+
+            if (up_data_flag == 0) {
+                prepareMovieData();
+            }
+
+            return rootView;
+        }
+
+        private List<Movie> movieList = new ArrayList<>();
+        private RecyclerView recyclerView;
+        private MoviesAdapter mAdapter;
+
+        private void prepareMovieData() {
+
+
+            Movie movie = new Movie("Chicken Run", "Animation", "2000");
+            movieList.add(movie);
+
+            movie = new Movie("Back to the Future", "Science Fiction", "1985");
+            movieList.add(movie);
+
+            movie = new Movie("Raiders of the Lost Ark", "Action & Adventure", "1981");
+            movieList.add(movie);
+
+            movie = new Movie("Goldfinger", "Action & Adventure", "1965");
+            movieList.add(movie);
+
+            movie = new Movie("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+            movieList.add(movie);
+
+            mAdapter.notifyDataSetChanged();
+
+            up_data_flag=1;
         }
     }
 
@@ -136,8 +347,19 @@ public class Home extends AppCompatActivity {
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // Return a Pending_Works_Fragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    return Pending_Works_Fragment.newInstance();
+
+                case 1:
+                    return Finished_Works_Fragment.newInstance();
+
+                default:
+                    return Upcoming_Works_Fragment.newInstance();
+
+
+            }
         }
 
         @Override
