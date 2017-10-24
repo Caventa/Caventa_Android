ddd3edb79e6fc7a600122d487a00392bf6b66d8d
baneeishaque <baneeishaque@gmail.com>
2017-10-24 17:03:33 +0530

    Cardview

--- app/src/main/java/caventa/ansheer/ndk/caventa/Home.java
+++ app/src/main/java/caventa/ansheer/ndk/caventa/Home.java
@@ -1,5 +1,6 @@
 package caventa.ansheer.ndk.caventa;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -61,8 +62,11 @@ public class Home extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+                Intent i=new Intent(Home.this,MainActivity.class);
+                startActivity(i);
             }
         });
 
